18c835ba22e5120cfaf1085c5f8a16dc583cb8fe
ceruleanox <Ceruleanox@gmail.com>
2020-11-08 09:39:25 -0500

    added sentiment analysis on back end

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ import pandas as pd
 import requests
 import csv
 import talib
+import matplotlib.pyplot as plt, mpld3
 
 from flask import Flask, render_template, request
 from patterns import patterns
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
 
 app = Flask(__name__)
 
@@ -23,11 +28,12 @@ def displayPatterns():
     
     pattern = request.args.get('pattern', None)
     stocks = {}
+    finviz_url = 'https://finviz.com/quote.ashx?t='
     
     with open('datasets/sp500companies_sample.csv') as f:
         for row in csv.reader(f):
             stocks[row[0]] = {'company': row[1]}
-
+ 
     if pattern:
     
         datafiles = os.listdir('datasets/daily')
@@ -37,7 +43,7 @@ def displayPatterns():
             df = pd.read_csv('datasets/daily/{}'.format(datasetName))
             pattern_function = getattr(talib, pattern) # allows us to pass name of function to variable and call function later instead of talib.CDLENGULFING
             symbol = datasetName.split('.')[0]
-
+            
             try:
                 result = pattern_function(df['Open'], df['High'], df['Low'], df['Close']) # non-zero results indicate pattern exists
                 # print(result)
@@ -45,15 +51,145 @@ def displayPatterns():
                 
                 if last > 0:
                     stocks[symbol][pattern] = 'bullish'
+
+                    ### BEGIN SENTIMENT ANAYLSIS ###
+                    url = finviz_url + symbol
+                    news_tables = {}
+
+                    req = Request(url=url, headers={'user-agent': 'my-app'})
+                    response = urlopen(req)
+
+                    html = BeautifulSoup(response, features='html.parser')
+                    news_table = html.find(id='news-table')
+                    news_tables[symbol] = news_table
+
+                    parsed_data = []
+
+                    for symbol, news_table in news_tables.items():
+
+                        for row in news_table.findAll('tr'):
+
+                            title = row.a.text
+                            date_data = row.td.text.split(' ')
+
+                            if len(date_data) == 1: #ex: 02:00AM
+                                time = date_data[0]
+                            else: # Nov-08-20 02:00AM
+                                date = date_data[0]
+                                time = date_data[1]
+
+                            parsed_data.append([symbol, date, time, title])
+
+                            df = pd.DataFrame(parsed_data, columns=['symbol', 'date', 'time', 'title'])
+                            # print(df)
+
+                            vader = SentimentIntensityAnalyzer()
+
+                            f = lambda title: vader.polarity_scores(title)['compound']
+                            df['compound'] = df['title'].apply(f)
+                            df['date'] = pd.to_datetime(df.date).dt.date
+                            print(df.tail(1))
+
+                            '''
+                            lastRow = len(df.index)-1
+                            totalSum = df.sum(axis=0)
+                            latestSentiment = totalSum.iloc[0,3]/lastRow
+                            
+                            print('*****')
+                            print('Overall sentiment on a scale of -1 (negative) to 1 (positive)')
+                            print(symbol)
+                            print(latestSentiment)
+                            print('*****')
+                            '''
+                        '''
+                        plt.figure(figsize=(10,8))
+                        mean_df = df.groupby(['symbol', 'date']).mean().unstack()
+                        mean_df = mean_df.xs('compound', axis="columns")
+                        mean_df.plot(kind='bar')
+                        mpld3.show()
+                        '''
+
+                            ### END SENTIMENT ANALYSIS ###
+    
                 elif last < 0:
                     stocks[symbol][pattern] = 'bearish'
+
+                    ### BEGIN SENTIMENT ANAYLSIS ###
+                    url = finviz_url + symbol
+                    news_tables = {}
+
+                    req = Request(url=url, headers={'user-agent': 'my-app'})
+                    response = urlopen(req)
+
+                    html = BeautifulSoup(response, features='html.parser')
+                    news_table = html.find(id='news-table')
+                    news_tables[symbol] = news_table
+
+                    parsed_data = []
+
+                    for symbol, news_table in news_tables.items():
+
+                        for row in news_table.findAll('tr'):
+
+                            title = row.a.text
+                            date_data = row.td.text.split(' ')
+
+                            if len(date_data) == 1: #ex: 02:00AM
+                                time = date_data[0]
+                            else: # Nov-08-20 02:00AM
+                                date = date_data[0]
+                                time = date_data[1]
+
+                            parsed_data.append([symbol, date, time, title])
+
+                            df = pd.DataFrame(parsed_data, columns=['symbol', 'date', 'time', 'title'])
+                            # print(df)
+
+                            vader = SentimentIntensityAnalyzer()
+
+                            f = lambda title: vader.polarity_scores(title)['compound']
+                            df['compound'] = df['title'].apply(f)
+                            df['date'] = pd.to_datetime(df.date).dt.date
+                            print(df.tail(1))
+
+                            '''
+                            lastRow = len(df.index)-1
+                            totalSum = df.sum(axis=0)
+                            latestSentiment = totalSum.iloc[0,3]/lastRow
+                            print('*****')
+                            print('Overall sentiment on a scale of -1 (negative) to 1 (positive)')
+                            print(symbol)
+                            print(latestSentiment)
+                            print('*****')
+                            '''
+                        '''                        lastRow = len(df.index)-1
+                        totalSum = df.sum(axis=0)
+                        latestSentiment = totalSum.iloc[0,3]/lastRow
+                        print('*****')
+                        print('Overall sentiment on a scale of -1 (negative) to 1 (positive)')
+                        print(latestSentiment)
+                        print('*****')
+                        '''                        
+                        '''
+                        plt.figure(figsize=(10,8))
+                        mean_df = df.groupby(['ticker', 'date']).mean().unstack()
+                        mean_df = mean_df.xs('compound', axis="columns")
+                        mean_df.plot(kind='bar')
+                        plt.show()
+                        '''
+                        ### END SENTIMENT ANALYSIS ###
+
                 else:
                     stocks[symbol][pattern] = None
 
                 # if last != 0:
                 #    print("{} triggered {}".format(filename, pattern))
+
+                print(df)
+
             except:
                 pass
+
     return render_template('findPatterns.html', patterns=patterns, stocks=stocks, currentPattern=pattern)
 
 @app.route('/snapshot')
